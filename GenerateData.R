94eca088f0eaf1ed0f476b635029cfc03d688093
obryludivine <obry.ludivine@gmail.com>
2022-08-13 10:44:57 +0200

    upadta

--- GenerateData.R
+++ GenerateData.R
@@ -9,7 +9,7 @@ geno_calc <- function(NbInd,vecMAF){
 }
 
 maf_calc <- function(SNP) {
-  # Allows to calculate the MAF of a SNP
+  # Allows to compute the MAF of a SNP
   freq_A <- (sum(SNP==0)*2+sum(SNP==1))/(2*length(SNP))
   MAF <- min(freq_A, 1-freq_A)
   return(MAF)
@@ -29,10 +29,10 @@ binary_association_test <- function(phenotype,SNP_geno){
 
 generate_ind_data <- function(nb_iteration=500, Design="quantitative",ScenarioBeta="reference", size_n=2000, size_m=8000, m1=50, R2=r2){
   # Allows to generate pvalues and covariates in the case of independent data (without correlation between SNPs)
-
+  
   reg_covariates<-list()
   reg_pvalues<-list()
-
+  
   for(iteration in c(1:nb_iteration)){
     #####################################
     #          Data generation          #
@@ -50,19 +50,19 @@ generate_ind_data <- function(nb_iteration=500, Design="quantitative",ScenarioBe
     theoretical_maf_subset_m1_1 <- runif(subset_m1_1,0.05,0.15)
     theoretical_maf_subset_m1_2 <- runif(subset_m1_2,0.15,0.25)
     theoretical_maf_subset_m1_3 <- runif(subset_m1_3,0.3,0.4)
-
+    
     ## Regarding genotypes
     geno_matrix_m0 <- apply(as.matrix(theoretical_maf_m0), 1, geno_calc,NbInd=size_n)
     geno_matrix_subset_m1_1 <- apply(as.matrix(theoretical_maf_subset_m1_1), 1, geno_calc,NbInd=size_n)
     geno_matrix_subset_m1_2 <- apply(as.matrix(theoretical_maf_subset_m1_2), 1, geno_calc,NbInd=size_n)
     geno_matrix_subset_m1_3 <- apply(as.matrix(theoretical_maf_subset_m1_3), 1, geno_calc,NbInd=size_n)
     matrix_X <- cbind(geno_matrix_subset_m1_1,geno_matrix_subset_m1_2,geno_matrix_subset_m1_3,geno_matrix_m0)
-
+    
     covariates <- apply(matrix_X, 2, maf_calc) # Computation of the estimated MAF = covariates
-
+    
     ## Regarding p-values
     if(Design=="quantitative"){ # If quantitative design
-      R2<-r2 # Percentage explaining the model
+      R2<-r2 # Percentage explaining the model coefficient of determination
       ### Beta building
       if(ScenarioBeta=="reference"){
         beta <- as.matrix(c(rep(3,subset_m1_1),rep(2,subset_m1_2),rep(1,subset_m1_3), rep(0,m0)))
@@ -73,7 +73,7 @@ generate_ind_data <- function(nb_iteration=500, Design="quantitative",ScenarioBe
       if(ScenarioBeta=="constant"){
         beta <- as.matrix(c(rep(2,subset_m1_1),rep(2,subset_m1_2),rep(2,subset_m1_3), rep(0,m0)))
       }
-
+      
       ### Regarding p-values
       XB <- matrix_X %*% beta # Epsilon Matrix (quantitative)
       sigma2 <- ( (r2-1) * sum((XB-mean(XB))^2) ) / ( r2*(2-size_n) )
@@ -91,16 +91,15 @@ generate_ind_data <- function(nb_iteration=500, Design="quantitative",ScenarioBe
       if(ScenarioBeta=="constant"){
         beta <- as.matrix(c(rep(log(1.5),subset_m1_1),rep(log(1.5),subset_m1_2),rep(log(1.5),subset_m1_3), rep(log(1),m0)))
       }
-
+      
       ### Choice of bêta0
-      list_ratio <- c(); i<-1;
+      list_ratio <- c()
       possible_values_beta0<-seq(0,-50,-0.1)
       for (beta0 in possible_values_beta0) {
         eXB <- exp(beta0+matrix_X%*%beta)
         Y <- rbinom(size_n, 1, (eXB/ (1+eXB)) )
         Y <- as.factor(Y)
-        list_ratio[i]<-c(prop.table(table(Y))[2])
-        i <- i+1
+        list_ratio[(length(list_ratio)+1)]<-c(prop.table(table(Y))[2])
       }
       beta0 <- possible_values_beta0[order(abs(list_ratio-0.5))[1]]
       eXB <- exp(beta0+matrix_X%*%beta)
@@ -108,11 +107,11 @@ generate_ind_data <- function(nb_iteration=500, Design="quantitative",ScenarioBe
       Y <- as.factor(Y)
       pvalues <- apply(matrix_X, 2, binary_association_test,phenotype=Y) # Computation of case_control Pj = Pvalues
     }
-
+    
     reg_covariates[[iteration]]<-covariates
     reg_pvalues[[iteration]]<-pvalues
   }
-
+  
   return(list(reg_pvalues, reg_covariates))
 }
 
@@ -128,82 +127,100 @@ discretise_geno <- function(SNP){
   return(genotype)
 }
 
-generate_corr_data <- function(nb_iteration=500, Design="quantitative", size_n=2000, size_m=8000, m1=50, blsize=10, vrho=0.20, R2=0.2){
+generate_corr_data <- function(nb_iteration=500, Design="quantitative", ScenarioBeta="reference", size_n=2000, size_m=8000, m1=50, blsize=10, vrho=0.20, R2=0.2){
   # Allows to generates p-values and covariates in the case of correlated data (with correlation between SNPs)
-
+  
   library(Matrix)
   library(dplyr)
   library(mvtnorm)
-
-
-  blsize <- 10
+  
+  #####################################
+  #          Data generation          #
+  #####################################
+  # Design: Choice between quantitative or case_control design to simulate pvalues
+  # size_n: Nb of individuals
+  # size_m: Nb of SNPs
+  # m1: Nb of causal SNPs
+  # blsize : bloc size
+  
+  blsize <- 10 
   nblock <- size_m/blsize
   mylist <- list()
   mysubmat <- matrix(vrho,blsize,blsize)
   diag(mysubmat) <- 1
   for(i in 1:nblock) mylist[[i]] <- mysubmat
   vsigma <- as.matrix(bdiag(mylist))
-
+  
+  m0<-size_m-m1 #  m0: Nb of non causal SNPs
+  subset_m1_1<-m1%/%3 # Nb of hypotheses in subgroup 1
+  subset_m1_2<-m1%/%3 # Nb of hypotheses in subgroup 2
+  subset_m1_3<-(m1%%3)+(m1%/%3) # Nb of hypotheses in subgroup 3
+  
+  ## Regarding covariables
+  theoretical_maf_m0 <- runif(m0, 0.05, 0.5)
+  theoretical_maf_subset_m1_1 <- runif(subset_m1_1,0.05,0.15)
+  theoretical_maf_subset_m1_2 <- runif(subset_m1_2,0.15,0.25)
+  theoretical_maf_subset_m1_3 <- runif(subset_m1_3,0.3,0.4)
+  total_theoretical_maf <- c(theoretical_maf_subset_m1_1,
+                             theoretical_maf_subset_m1_2,
+                             theoretical_maf_subset_m1_3,
+                             theoretical_maf_m0)
+  
+  mat1<-rmvnorm(size_n,mean=rep(0,size_m/4),sigma = vsigma[1:2000,1:2000])
+  mat2<-rmvnorm(size_n,mean=rep(0,size_m/4),sigma = vsigma[2001:4000,2001:4000])
+  mat3<-rmvnorm(size_n,mean=rep(0,size_m/4),sigma = vsigma[4001:6000,4001:6000])
+  mat4<-rmvnorm(size_n,mean=rep(0,size_m/4),sigma = vsigma[6001:8000,6001:8000])
+  
+  matbis1 <- rbind(total_theoretical_maf[1:2000],mat1)
+  matbis2 <- rbind(total_theoretical_maf[2001:4000],mat2)
+  matbis3 <- rbind(total_theoretical_maf[4001:6000],mat3)
+  matbis4 <- rbind(total_theoretical_maf[6001:8000],mat4)
+  
+  matrix_X <- apply(cbind(matbis1, matbis2, matbis3, matbis4),
+                    2, discretise_geno)
+  
+  covariates <- apply(matrix_X, 2, maf_calc) # Computation of the estimated MAF = covariates
+  
+  # Cutting the MAF into subgroups
+  SNPs_maf_subgp_m1_1 <- which(covariates>0.05&covariates<0.15)
+  SNPs_maf_subgp_m1_2 <- which(covariates>0.15&covariates<0.25)
+  SNPs_maf_subgp_m1_3 <- which(covariates>0.30&covariates<0.50)
+  
+  # How to define the True Positives (TP) ?
+  ## Define the correlation matrix
+  mcor<-as.data.frame(abs(cor(matrix_X,matrix_X)))
+  mcor_transit<-matrix(0,ncol=size_m,nrow=size_m)
+  mcor_transit[which(upper.tri(mcor_transit,T),arr.ind=TRUE)[,2:1]]<-c(mcor[which(upper.tri(mcor,T),arr.ind=TRUE)[,2:1]])
+  correlation_matrix<-mcor_transit
+  
   reg_covariates<-list()
   reg_pvalues<-list()
   List_SNPs_Drawn_Correlated<-list()
-  correlation_draw<-list()
+  reg_draw<-list()
   for(iteration in c(1:nb_iteration)){
-    #####################################
-    #          Data generation          #
-    #####################################
-    # Design: Choice between quantitative or case_control design to simulate pvalues
-    # size_n: Nb of individuals
-    # size_m: Nb of SNPs
-    # m1: Nb of causal SNPs
-    m0<-size_m-m1 #  m0: Nb of non causal SNPs
-    subset_m1_1<-m1%/%3 # Nb of hypotheses in subgroup 1
-    subset_m1_2<-m1%/%3 # Nb of hypotheses in subgroup 2
-    subset_m1_3<-(m1%%3)+(m1%/%3) # Nb of hypotheses in subgroup 3
-
-    ## Regarding covariables
-    theoretical_maf_m0 <- runif(m0, 0.05, 0.5)
-    theoretical_maf_subset_m1_1 <- runif(subset_m1_1,0.05,0.15)
-    theoretical_maf_subset_m1_2 <- runif(subset_m1_2,0.15,0.25)
-    theoretical_maf_subset_m1_3 <- runif(subset_m1_3,0.3,0.4)
-    total_theoretical_maf <- c(theoretical_maf_subset_m1_1,
-                               theoretical_maf_subset_m1_2,
-                               theoretical_maf_subset_m1_3,
-                               theoretical_maf_m0)
-
-    mat1<-rmvnorm(size_n,mean=rep(0,size_m/4),sigma = vsigma[1:2000,1:2000])
-    mat2<-rmvnorm(size_n,mean=rep(0,size_m/4),sigma = vsigma[2001:4000,2001:4000])
-    mat3<-rmvnorm(size_n,mean=rep(0,size_m/4),sigma = vsigma[4001:6000,4001:6000])
-    mat4<-rmvnorm(size_n,mean=rep(0,size_m/4),sigma = vsigma[6001:8000,6001:8000])
-
-    matbis1 <- rbind(total_theoretical_maf[1:2000],mat1)
-    matbis2 <- rbind(total_theoretical_maf[2001:4000],mat2)
-    matbis3 <- rbind(total_theoretical_maf[4001:6000],mat3)
-    matbis4 <- rbind(total_theoretical_maf[6001:8000],mat4)
-
-    matrix_X <- apply(cbind(matbis1, matbis2, matbis3, matbis4),
-                      2, discretise_geno)
-
-    covariates <- apply(matrix_X, 2, maf_calc) # Computation of the estimated MAF = covariates
-
-    # Cutting the MAF into subgroups
-    SNPs_maf_subgp_m1_1 <- which(covariates>0.05&covariates<0.15)
-    SNPs_maf_subgp_m1_2 <- which(covariates>0.15&covariates<0.25)
-    SNPs_maf_subgp_m1_3 <- which(covariates>0.30&covariates<0.50)
-
+    
+    ## Beta initialization
+    beta <- c(rep(0,size_m))
+    
     ## Draw among them
     draw1<-sample(SNPs_maf_subgp_m1_1,m1%/%3)
     draw2<-sample(SNPs_maf_subgp_m1_2,m1%/%3)
     draw3<-sample(SNPs_maf_subgp_m1_3,m1-2*(m1%/%3))
     draw<-c(draw1,draw2,draw3)
-
-    ## Beta initialization
-    beta <- c(rep(0,size_m))
-
+    
     ## Regarding p-values
     if(Design=="quantitative"){ # If quantitative design
-      r2 <- R2 # Percentage explaining the model
-      beta[draw1] <- 3; beta[draw2] <- 2; beta[draw3] <- 1 # Beta for each group
+      r2 <- R2 # Percentage explaining the model (coefficient of determination)
+      ### Beta building
+      if(ScenarioBeta=="reference"){
+        beta[draw1] <- 3; beta[draw2] <- 2; beta[draw3] <- 1 # Beta for each group
+      }
+      if(ScenarioBeta=="inverse"){
+        beta[draw1] <- 1; beta[draw2] <- 2; beta[draw3] <- 3 # Beta for each group
+      }
+      if(ScenarioBeta=="constant"){
+        beta[draw1] <- 2; beta[draw2] <- 2; beta[draw3] <- 2 # Beta for each group
+      }
       beta <- as.matrix(unlist(beta)) # Final Beta vector
       ### Regarding p-values
       XB <- matrix_X %*% beta # Epsilon Matrix (quantitative)
@@ -212,17 +229,25 @@ generate_corr_data <- function(nb_iteration=500, Design="quantitative", size_n=2
       Y <- XB + epsilon # Computation of Y (quantitative)
       pvalues <- apply(matrix_X, 2, quantitative_association_test,phenotype=Y) # Computation of quantitative Pj = Pvalues
     } else if(Design=="case_control"){ # If case_control design
-      beta[draw1] <- log(1.8); beta[draw2] <- log(1.5); beta[draw3] <- log(1.3) # Beta for each group
+      ### Beta building
+      if(ScenarioBeta=="reference"){
+        beta[draw1] <- log(1.8); beta[draw2] <- log(1.5); beta[draw3] <- log(1.3) # Beta for each group
+      }
+      if(ScenarioBeta=="inverse"){
+        beta[draw1] <- log(1.3); beta[draw2] <- log(1.5); beta[draw3] <- log(1.8) # Beta for each group
+      }
+      if(ScenarioBeta=="constant"){
+        beta[draw1] <- log(1.5); beta[draw2] <- log(1.5); beta[draw3] <- log(1.5) # Beta for each group
+      }
       beta <- as.matrix(unlist(beta)) # Final Beta vector
       ### Choice of bêta0
-      list_ratio <- c(); i <- 1;
+      list_ratio <- c()
       possible_values_beta0<-seq(0,-50,-0.1)
       for (beta0 in possible_values_beta0) {
         eXB <- exp(beta0+matrix_X%*%beta)
         Y <- rbinom(size_n, 1, (eXB/ (1+eXB)) )
         Y <- as.factor(Y)
-        list_ratio[i]<-c(prop.table(table(Y))[2])
-        i <- i+1
+        list_ratio[(length(list_ratio)+1)]<-c(prop.table(table(Y))[2])
       }
       beta0 <- possible_values_beta0[order(abs(list_ratio-0.5))[1]]
       eXB <- exp(beta0+matrix_X%*%beta)
@@ -230,87 +255,108 @@ generate_corr_data <- function(nb_iteration=500, Design="quantitative", size_n=2
       Y <- as.factor(Y)
       pvalues <- apply(matrix_X, 2, binary_association_test,phenotype=Y) # Computation of case_control Pj = Pvalues
     }
-
-    # How to define the True Positives (TP) ?
-    ## Define the correlation matrix
-    mcor<-as.data.frame(abs(cor(matrix_X,matrix_X)))
-    correlation_draw[[iteration]]<-subset(mcor,select = c(draw1,draw2,draw3))
+    
     ## Define the list of drawn correlated SNPs
     List_SNPs_Drawn_Correlated[[iteration]] <- lapply(draw,correlationsofSNPs,
-                                                      CorrMatrix=mcor, Threshold=(vrho-0.05))
+                                                      CorrMatrix=mcor, Threshold=(vrho))
     reg_covariates[[iteration]]<-covariates
     reg_pvalues[[iteration]]<-pvalues
+    reg_draw[[iteration]]<-draw
   }
-
-  return(list(reg_pvalues, reg_covariates, List_SNPs_Drawn_Correlated,correlation_draw))
+  
+  return(list(reg_pvalues, reg_covariates, List_SNPs_Drawn_Correlated,correlation_matrix, reg_draw))
 }
 
 correlationsofSNPs<-function(List_Drawn_SNPs, CorrMatrix, Threshold){
   # Allows you to select the correlated SNPs with the drawn SNPs
-  Corr_SNPs<-which(CorrMatrix[,List_Drawn_SNPs]>Threshold)
+  Corr_SNPs<-which(CorrMatrix[,List_Drawn_SNPs]>=Threshold)
   return(Corr_SNPs)
 }
 
 #Parameters
 nb_iteration <- 1
 size_n <- 2000
-r2 <- 0.2
-path_out <- c("/home/lobry/Documents/Nouveau2021/Test/")
+r2 <- 0.2 #coefficient of determination
+path_out <- c("/media/lobry/Elements/TESTADELETE/")
 
 #Main
 for (Case in c("independent","correlation")) {
-  if(Case==c("independent")){
-    vrho <- 0
-    blsize <- 0
+  if(Case==c("independent")){ #case without correlations between SNPs
+    vrho <- 0 #rho value
+    blsize <- 0 #bloc size
     for (scenario in c("reference","inverse","constant")) {
+      if(scenario=="reference"){ #corresponding to scenario 1
+        binary_beta=c("B_1.8_1.5_1.3_1.rda")
+        quantitative_beta=c("B3210.rda")
+      }
+      if(scenario=="inverse"){ #corresponding to scenario 2
+        binary_beta=c("B_1.3_1.5_1.8_1.rda")
+        quantitative_beta=c("B1230.rda")
+      }
+      if(scenario=="constant"){ #corresponding to scenario 3
+        binary_beta=c("B_1.5_1.5_1.5_1.rda")
+        quantitative_beta=c("B2220.rda")
+      }
       for (design in c("quantitative","case_control")) {
         for (size_m in c(8000,14000,20000)) {
-          for(m1 in c(25,50,100,150)){
+          #size_m is the total number of null hypotheses tested
+          for(m1 in c(5,10,15,20,25,50,100,150)){
+            #m1 is the number of causal variants
             Config <- generate_ind_data(nb_iteration=nb_iteration,Design=design,ScenarioBeta=scenario,size_n=size_n,size_m=size_m,m1=m1,R2=r2)
             reg_pvalues <- Config[[1]]
             reg_covariates <- Config[[2]]
             if (design=="quantitative") {
-              if(scenario=="reference"){quantitative_beta <- c("B3210.RData")}
-              if(scenario=="inverse"){quantitative_beta <- c("B1230.RData")}
-              if(scenario=="constant"){quantitative_beta <- c("B2220.RData")}
-
               file_out <- paste("LineR","n",size_n,"m",size_m,"m1",m1,"rho",vrho,"tb",blsize,"r2",r2,quantitative_beta,sep="_")
             }
             if(design=="case_control"){
-              if(scenario=="reference"){binary_beta <- c("B_1.8_1.5_1.3_1.RData")}
-              if(scenario=="inverse"){binary_beta <- c("B_1.3_1.5_1.8_1.RData")}
-              if(scenario=="constant"){binary_beta <- c("B_1.5_1.5_1.5_1.RData")}
-
               file_out <- paste("Logit","n",size_n,"m",size_m,"m1",m1,"rho",vrho,"tb",blsize,binary_beta,sep="_")
             }
             save(reg_covariates,reg_pvalues, file=paste(path_out,file_out,sep=""))
-            print("fin")
           }
         }
       }
     }
   }
-  if(Case==c("correlation")){
-    size_m <- 8000
-    m1 <- 50
-    blsize <- 10
-    for (design in c("case_control","quantitative")) {
-      for (vrho in c(0.10,0.20,0.35,0.5,0.75)) {
-          Config <- generate_corr_data(nb_iteration=nb_iteration,Design=design,
-                                               size_n=size_n,size_m=size_m,m1=m1,blsize=blsize,vrho=vrho,R2=r2)
-          reg_pvalues <- Config[[1]]
-          reg_covariates <- Config[[2]]
-          reg_SNPs_Drawn_Correlated <- Config[[3]]
-          reg_correlation_draw <- Config[[4]]
-          if (design=="quantitative") {
-            file_out <- paste("LineR","n",size_n,"m",size_m,"m1",m1,"rho",vrho,"tb",blsize, "r2",r2, "B3210.RData",sep="_")
+  if(Case==c("correlation")){ #case with correlations between SNPs
+    size_m <- 8000 #total number of null hypotheses tested
+    blsize <- 10 #bloc size
+    for (scenario in c("reference","inverse","constant")) {
+      if(scenario=="reference"){ #corresponding to scenario 1
+        binary_beta=c("B_1.8_1.5_1.3_1.rda")
+        quantitative_beta=c("B3210.rda")
+      }
+      if(scenario=="inverse"){ #corresponding to scenario 2
+        binary_beta=c("B_1.3_1.5_1.8_1.rda")
+        quantitative_beta=c("B1230.rda")
+      }
+      if(scenario=="constant"){ #corresponding to scenario 3
+        binary_beta=c("B_1.5_1.5_1.5_1.rda")
+        quantitative_beta=c("B2220.rda")
+      }
+      for (design in c("case_control","quantitative")) {
+        for(m1 in c(5,10,15,20,25,50,100,150)){
+          #m1 is the number of causal variants
+          for (vrho in c(0.10,0.20,0.35,0.5,0.75)) {
+            #vrho is the correlation value between variants
+            
+            Config <- generate_corr_data(nb_iteration=nb_iteration,Design=design,ScenarioBeta=scenario,
+                                         size_n=size_n,size_m=size_m,m1=m1,blsize=blsize,vrho=vrho,R2=r2)
+            reg_pvalues <- Config[[1]]
+            reg_covariates <- Config[[2]]
+            reg_SNPs_Drawn_Correlated <- Config[[3]]
+            reg_correlation_all <- Config[[4]]
+            reg_draw <- Config[[5]]
+            
+            if (design=="quantitative") {
+              file_out <- paste("LineR","n",size_n,"m",size_m,"m1",m1,"rho",vrho,"tb",blsize, "r2",r2, quantitative_beta ,sep="_")
+            }
+            if(design=="case_control"){
+              file_out <- paste("Logit","n",size_n,"m",size_m,"m1",m1,"rho",vrho,"tb",blsize, binary_beta,sep="_")
+            }
+            save(reg_covariates,reg_pvalues,reg_SNPs_Drawn_Correlated,reg_correlation_all, reg_draw, file=paste(path_out,file_out,sep=""))
           }
-          if(design=="case_control"){
-            file_out <- paste("Logit","n",size_n,"m",size_m,"m1",m1,"rho",vrho,"tb",blsize, "B_1.8_1.5_1.3_1.RData",sep="_")
-          }
-          save(reg_covariates,reg_pvalues,reg_SNPs_Drawn_Correlated,reg_correlation_draw, file=paste(path_out,file_out,sep=""))
-          print("end")
         }
+      }
     }
   }
 }
